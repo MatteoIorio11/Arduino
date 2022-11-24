5c9c95e914c59cc31fc2223b210c5524800ebf6a
Matteo Iorio - matteo.iorio2@studio.unibo.it <matteo.iorio2@studio.unibo.it>
2022-11-24 09:53:03 +0100

    added the esp32

--- laboratorio-8/src/main.cpp
+++ laboratorio-8/src/main.cpp
@@ -1,9 +1,15 @@
 #include <Arduino.h>
-
+#include "Servo.h"
+Servo s;
 void setup() {
-  // put your setup code here, to run once:
+  s.attach(9);
+  Serial.begin(9600);
 }
 
 void loop() {
   // put your main code here, to run repeatedly:
+  long readValue = analogRead(A0);
+  Serial.println(readValue);
+  int value = map(readValue, 0, 1023, 0, 180);
+  s.write(value);
 }
